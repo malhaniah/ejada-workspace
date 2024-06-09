513d13d53ccd4a5d0e673085827b38abbec55952
malhaniah <mohd.alhaniah@gmail.com>
2024-06-09 13:43:10 +0300

    Added partners page

---
+++ README.md
@@ -0,0 +1,3 @@
+### DEVELOPER NOTES
+- Plan to use tailwind as solution for plain css headache
+- Reorgnize html,css files for customization and maintain
--- index.js
+++ index.js
@@ -4,3 +4,14 @@
     $(this).addClass("active");
   });
 })();
+
+(function toggleCategoriesActive() {
+  $(".partners-categories .list-item").on("click", function () {
+    $(".partners-categories .list-item").removeClass("active");
+    $(this).addClass("active");
+    $(".partners-categories .list-item .active-rounded").removeClass("active");
+    $(this).find(".active-rounded").addClass("active");
+    $(".partners-categories .list-item .inactive-rounded").addClass("active");
+  });
+
+})();
